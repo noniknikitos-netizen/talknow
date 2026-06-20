e006e5ff90e905e403c59cb9f7a4f470deba0b98
noniknikitos-netizen <noniknikitos@gmail.com>
2026-06-20 12:16:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ function findPartner(ws) {
 
     if (partner) {
         waitingUsers = waitingUsers.filter(user => user !== partner);
-        
         ws.partner = partner;
         partner.partner = ws;
 
@@ -76,5 +75,5 @@ function disconnectPartner(ws) {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Сервер запущено на порту ${PORT}`);
+    console.log(`Сервер працює на порту ${PORT}`);
 });
